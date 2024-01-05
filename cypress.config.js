a3dff1c1486c19fca045223aafdd79926c417e3a
Semen Syzov <ssyzov@deloitte.com>
2024-01-05 11:50:20 +0200

    add lecture 16

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  env: {
+    config_env: 'config env variable value',
+  },
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx,mjs}',
     baseUrl: 'https://www.akveo.com/ngx-admin/',
     watchForFileChanges: false,
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
   },
 });
  
---
+++ cypress/fixtures/random.js
@@ -0,0 +1,5 @@
+module.exports = { 
+  oneToHundred: Math.round(Math.random() * 100) + 1, 
+  oneToTen: Math.round(Math.random() * 10) + 1
+}
+
